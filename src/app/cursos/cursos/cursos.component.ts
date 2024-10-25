9ae35e80eda775990780f8db0024c5e9dd2393c4
amauri-castro <amauri.p.castro@gmail.com>
2024-10-24 23:04:52 -0300

    implementado consulta a partir de arquivo json usando operadores do RxJs no service de cursos

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../model/curso';
 import { CursosService } from '../services/cursos.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
@@ -9,13 +10,14 @@ import { CursosService } from '../services/cursos.service';
 })
 export class CursosComponent implements OnInit {
 
-  cursos: Curso[] = [];
+  cursos: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
-  constructor(private cursosService: CursosService) { }
+  constructor(private cursosService: CursosService) {
+    this.cursos = this.cursosService.list();
+   }
 
   ngOnInit(): void {
-    this.cursos = this.cursosService.list();
   }
 
 }
