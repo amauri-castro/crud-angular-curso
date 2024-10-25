a8d1572a42531e33e0a4e520b1b6435ad0ea893e
amauri-castro <amauri.p.castro@gmail.com>
2024-10-25 10:03:35 -0300

    Criado modulo compartilhado e componente erro dialog

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../model/curso';
 import { CursosService } from '../services/cursos.service';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-cursos',
@@ -13,9 +15,24 @@ export class CursosComponent implements OnInit {
   cursos$: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
-  constructor(private cursosService: CursosService) {
-    this.cursos$ = this.cursosService.list();
+  constructor(private cursosService: CursosService,
+    public dialog: MatDialog
+  ) {
+    this.cursos$ = this.cursosService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos')
+        return of([])
+      })
+    );
    }
+
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
 
   ngOnInit(): void {
   }
